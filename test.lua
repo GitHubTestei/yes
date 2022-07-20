41253515ef1ab45a738078100c987b566db906ee
GitHubTestei <109692029+GitHubTestei@users.noreply.github.com>
2022-07-20 11:30:13 -0700

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,59 @@
+-- Sheet for Rondo Alla Turca by Wolfgang Amadeus Mozart
+
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 9*60 + 25 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- tempo overides the delay
+
+shared.scr = [[
+[18wtuos] [269ey] [370wruo] 
+[18wtuo] [269ey] [370wruo]
+[18wtu] [29ey] [370wru]  
+[59wryo] [7ry] [18wtu] 
+[18f] [29d] [30a] p o 
+[18f] [29d] [30a] 
+[18f] [29d] [30a] p o p
+[59wrya] [7rd] [18wtu] 
+[18f] 5 [29d] 6 [30a] 7 0 p o 
+[18f] 5 [29d] 6 [30a] 7 0  
+[18f] 5 [29d] 6 [30a] 7 0 p o p 
+[5wa] 9 w [7ryd] r [18u] 5 8 0 w [wtu] 
+[5wa] 9 w [7ryd] [18wtuf] 
+k h  1 5 8 0 w t u o - 2 6 9 e y  -3 7 0 w r u o - 
+j h j h  1 5 8 0 w t u o - 2 6 9 e y  -3 7 0 w r u o - 
+h j [1k] - [5k] [8k] [0k] [wk] - [2j] [6j] [9j] [3j] - [7h] [0f] w [wruo] 
+f - h j k - [59wryoak] [7rdz] [18wtuf] 
+k h - 1 5 8 0 w t u o - 2 6 9 e y  -3 7 0 w r u o - 
+j h j h  1 5 8 0 w t u o - 2 6 9 e y  -3 7 0 w r u o - 
+h j [1k] - [5k] [8k] [0k] [wk] - [2j] [6j] [9j] [3j] - [7h] [0f] w [wruo] 
+f - h j k - [59wryoak] [7rdz] [18wtuf]  --
+f [59wryoak] [7rdz] [18wtufx] 
+[18f] [29d] [30a] p o 
+[18f] [29d] {[30a] asa}
+[18f] [29d] [30a] p o p
+[59wrya] [7rd] [18wtu] → 
+[18f] [29d] [30a] 7 p o [0w] 
+[18f] [29d] [30a]  7 0 w r u 
+[18f] [29d] [30a] 7 p o p [0wru] 
+[59wrya] [7rd] [18wtu] {adfhjk}-- [59wryak] [7rdz] [18wtufx] {sfhlx}
+k h  1 5 8 0 w t u o - 2 6 9 e y  -3 7 0 [wk] [rk] [uj] [oh] -
+ j h j h 1 5 8 0w t u o - 2 6 9 e y - 3 7 0 w r u
+h j [1k] - [5k] [8k] [0k] [wk] - [2j] [6j] [9j] [3j] - [7h] [0f] w [wruo] 
+f - h j k - [59wryoak] [7rdz] [18wtuf] 
+k h - 1 5 8 9 0 -- 2 6 9 0 Q -- 3 7 0 w r 
+j h j h  1 5 8 9 0 w t u o - 2 6 9 0 Q e y  -3 7 0 w r u o - 
+h j [1k] - [5k] [8k] [0k] 9 [wk] - [2j] [6j] [9j] 0 Q [3j] - [7h] [0f] w [wruo] 
+f h j - [5k] 9 w r y  o a [7d] r [18f] w t u o s f h l x v m
+f [59wryoak] [7rdz] { [18wtudz] [fx] }
+1 5 8 9 0 9 8 5 1 5 8 9 0 9 8 5 1 
+2 6 9 Q e Q 9 6 26 9 Q e Q 9 6 2 
+3 7 0 w r u r w 0 7 3 7 0 w r u u r 0 7 3 
+[1h] 5 [8G] 9 [0h] 9 [8G] 5 [1h] [5G] 8 9 0 9 8 5 1 
+2 6 9 Q e → 3 7 0 w r u r w 0 7 3 7 0 w r u u r 0 7 3 
+[59wryh] [7ryG] [18wtudf] 
+k h  [wtu] [eyI] [ruo] → j h j h  [wtu] [eyI] [ruo] → 
+h j [wtuk] k k k [eyj] j j j [wh] - [rh] [uf] w r  u
+f - h j k - [wryk] [rd] [wtuf] 
+1 5 8 9 0 - 2 6 9 0 Q e - 3 7 0 w r u --- |
+5 9 w e r - 7 r [yI] → 1 5 [8u] w t u {sfhlxv}
